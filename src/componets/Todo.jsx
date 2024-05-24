dc11bfb73e58060dbf501c57d02f328b94e3af12
Robert Roger <robert.roger@digitalcollegesul.local>
2024-05-24 18:34:33 -0300

    first commit

---
+++ src/componets/Todo.jsx
@@ -0,0 +1,17 @@
+export default function Todo({todo}) {
+    return (
+        <>
+            <div className="todo">
+                <div className="content">
+                    <p>{todo.text}</p>
+                    <p className='category'>({todo.category})</p>
+                </div>
+
+                <div>
+                    <button className="complete">Completar</button>
+                    <button className="remove">x</button>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/componets/TodoForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function TodoForm({addTodo}) {
+
+    const [value, setValue] = useState("")
+    const [category, setCategory] = useState("")
+
+    const handleSubmit  = (e) => {
+        e.preventDefault()
+        if(!value || !category) return
+        console.log(value, category)
+        addTodo(value, category)
+        setValue("")
+        setCategory("")
+    }
+
+    return (
+        <div className="todo-form">
+            <h2>Criar tarefas:</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Digite o Título" onChange={(e)=> setValue(e.target.value)} />
+                <select value={category} onChange={(e)=> setCategory(e.target.value)}>
+                    <option value="">Selecione uma categoria</option>
+                    <option value="Trabalho">Trabalho</option>
+                    <option value="Pessoal">Pessoal</option>
+                    <option value="Estudos">Estudos</option>
+                </select>
+                <button type="submit">Criar Tarefa</button>
+            </form>
+        </div>
+    );
+}
